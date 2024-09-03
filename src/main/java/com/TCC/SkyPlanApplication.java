64c5ca8e52c880292c17124dddd2678b47322ac8
baruc <baruc.bm@hotmail.com>
2024-09-03 18:58:13 -0300

    Commit inicial

---
+++ src/main/java/com/TCC/SkyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.TCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TCC/SkyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
