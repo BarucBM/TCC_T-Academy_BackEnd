2ad27b8ffda71e6bfd93ea3d7453f2784ee7a9e4
Leticia <leticiaborchardt123@gmail.com>
2024-09-24 08:46:24 -0300

    fix: fix import of EventSpecification

--- src/main/java/com/TCC/services/EventService.java
+++ src/main/java/com/TCC/services/EventService.java
@@ -3,7 +3,7 @@ package com.TCC.services;
 import com.TCC.domain.event.Event;
 import com.TCC.domain.event.EventDTO;
 import com.TCC.repositories.EventRepository;
-import com.TCC.specification.EventSpecification;
+import com.TCC.specifications.EventSpecification;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
