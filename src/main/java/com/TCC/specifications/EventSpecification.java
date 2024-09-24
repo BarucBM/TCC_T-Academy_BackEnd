335651eb2a25a788fc15dfb82e5703e1fd7262c5
baruc <baruc.bm@hotmail.com>
2024-09-24 16:27:02 -0300

    feat: add Specification to filter the Get All events function

---
+++ src/main/java/com/TCC/specifications/EventSpecification.java
@@ -0,0 +1,27 @@
+package com.TCC.specifications;
+
+
+import com.TCC.domain.event.Event;
+import org.springframework.data.jpa.domain.Specification;
+
+public class EventSpecification {
+    public static Specification<Event> titleContains(String title){
+        return ((root, query, criteriaBuilder) ->
+                title == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("title")),"%" +title.toLowerCase()+ "%"));
+    }
+
+    public static Specification<Event> locationContains(String location){
+        return ((root, query, criteriaBuilder) ->
+                location == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("location")),"%" + location.toLowerCase() + "%"));
+    }
+
+    public static Specification<Event> descriptionContains(String description){
+        return ((root, query, criteriaBuilder) ->
+                description == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("description")),"%" + description.toLowerCase() + "%"));
+    }
+
+    public static Specification<Event> emailContains(String email){
+        return ((root, query, criteriaBuilder) ->
+                email == null ? null : criteriaBuilder.like(criteriaBuilder.lower(root.get("email")),"%" + email.toLowerCase() + "%"));
+    }
+}
