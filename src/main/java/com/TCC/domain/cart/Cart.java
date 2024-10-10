96d09886431d01812715cb915b2ab0bc81cfb06e
baruc <baruc.bm@hotmail.com>
2024-10-10 11:47:54 -0300

    feat: add cart identification

--- src/main/java/com/TCC/domain/cart/Cart.java
+++ src/main/java/com/TCC/domain/cart/Cart.java
@@ -1,9 +1,12 @@
 package com.TCC.domain.cart;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "cart")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
