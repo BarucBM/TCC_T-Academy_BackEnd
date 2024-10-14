7ddc4ec55ce17fdfaf7ddae4a630e103c3b4bed8
Leticia <leticiaborchardt123@gmail.com>
2024-10-14 13:57:22 -0300

    feat: create endpoints for editing and deleting events

--- src/main/java/com/TCC/domain/event/Event.java
+++ src/main/java/com/TCC/domain/event/Event.java
@@ -60,12 +60,12 @@ public class Event {
     )
     private List<Image> images;
 
-    @OneToMany(mappedBy = "event")
-    private List<Weather> weathers;
-
-    @OneToMany(mappedBy = "event")
-    private List<Notification> notifications;
-
-    @OneToMany(mappedBy = "event")
-    private List<UserEvent> usersEvent;
+//    @OneToMany(mappedBy = "event")
+//    private List<Weather> weathers;
+//
+//    @OneToMany(mappedBy = "event")
+//    private List<Notification> notifications;
+//
+//    @OneToMany(mappedBy = "event")
+//    private List<UserEvent> usersEvent;
 }
--- src/main/java/com/TCC/services/AddressService.java
+++ src/main/java/com/TCC/services/AddressService.java
@@ -3,6 +3,7 @@ package com.TCC.services;
 import com.TCC.domain.address.Address;
 import com.TCC.domain.address.AddressDTO;
 import com.TCC.repositories.AddressRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,26 @@ public class AddressService {
         this.addressRepository = addressRepository;
     }
 
+    public Address getAddressById(String id) {
+        return addressRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Address not found with ID: " + id));
+    }
+
     public Address createAddress(AddressDTO addressDTO) {
         Address address = new Address();
         BeanUtils.copyProperties(addressDTO, address);
 
         return addressRepository.save(address);
+    }
+
+    public Address updateAddress(String id, AddressDTO addressDTO) {
+        Address address = this.getAddressById(id);
+        BeanUtils.copyProperties(addressDTO, address);
+
+        return addressRepository.save(address);
+    }
+
+    public void deleteAddress(Address address) {
+        addressRepository.delete(address);
     }
 }
